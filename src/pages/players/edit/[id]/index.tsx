6645930c99ebbe8f7e6bb9211f592aa7ef041a17
roq-ai <>
2023-06-27 20:45:12 +0000

    Initial commit

---
+++ src/pages/players/edit/[id]/index.tsx
@@ -0,0 +1,140 @@
+import AppLayout from 'layout/app-layout';
+import React, { useState } from 'react';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Text,
+  Box,
+  Spinner,
+  FormErrorMessage,
+  Switch,
+  NumberInputStepper,
+  NumberDecrementStepper,
+  NumberInputField,
+  NumberIncrementStepper,
+  NumberInput,
+  Center,
+} from '@chakra-ui/react';
+import * as yup from 'yup';
+import DatePicker from 'react-datepicker';
+import { FiEdit3 } from 'react-icons/fi';
+import { useFormik, FormikHelpers } from 'formik';
+import { getPlayerById, updatePlayerById } from 'apiSdk/players';
+import { Error } from 'components/error';
+import { playerValidationSchema } from 'validationSchema/players';
+import { PlayerInterface } from 'interfaces/player';
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+import { AsyncSelect } from 'components/async-select';
+import { ArrayFormField } from 'components/array-form-field';
+import { AccessOperationEnum, AccessServiceEnum, withAuthorization } from '@roq/nextjs';
+import { UserInterface } from 'interfaces/user';
+import { OrganizationInterface } from 'interfaces/organization';
+import { getUsers } from 'apiSdk/users';
+import { getOrganizations } from 'apiSdk/organizations';
+
+function PlayerEditPage() {
+  const router = useRouter();
+  const id = router.query.id as string;
+  const { data, error, isLoading, mutate } = useSWR<PlayerInterface>(
+    () => (id ? `/players/${id}` : null),
+    () => getPlayerById(id),
+  );
+  const [formError, setFormError] = useState(null);
+
+  const handleSubmit = async (values: PlayerInterface, { resetForm }: FormikHelpers<any>) => {
+    setFormError(null);
+    try {
+      const updated = await updatePlayerById(id, values);
+      mutate(updated);
+      resetForm();
+      router.push('/players');
+    } catch (error) {
+      setFormError(error);
+    }
+  };
+
+  const formik = useFormik<PlayerInterface>({
+    initialValues: data,
+    validationSchema: playerValidationSchema,
+    onSubmit: handleSubmit,
+    enableReinitialize: true,
+    validateOnChange: false,
+    validateOnBlur: false,
+  });
+
+  return (
+    <AppLayout>
+      <Box bg="white" p={4} rounded="md" shadow="md">
+        <Box mb={4}>
+          <Text as="h1" fontSize="2xl" fontWeight="bold">
+            Edit Player
+          </Text>
+        </Box>
+        {error && (
+          <Box mb={4}>
+            <Error error={error} />
+          </Box>
+        )}
+        {formError && (
+          <Box mb={4}>
+            <Error error={formError} />
+          </Box>
+        )}
+        {isLoading || (!formik.values && !error) ? (
+          <Center>
+            <Spinner />
+          </Center>
+        ) : (
+          <form onSubmit={formik.handleSubmit}>
+            <FormControl id="profile_name" mb="4" isInvalid={!!formik.errors?.profile_name}>
+              <FormLabel>Profile Name</FormLabel>
+              <Input
+                type="text"
+                name="profile_name"
+                value={formik.values?.profile_name}
+                onChange={formik.handleChange}
+              />
+              {formik.errors.profile_name && <FormErrorMessage>{formik.errors?.profile_name}</FormErrorMessage>}
+            </FormControl>
+            <AsyncSelect<UserInterface>
+              formik={formik}
+              name={'user_id'}
+              label={'Select User'}
+              placeholder={'Select User'}
+              fetcher={getUsers}
+              renderOption={(record) => (
+                <option key={record.id} value={record.id}>
+                  {record?.email}
+                </option>
+              )}
+            />
+            <AsyncSelect<OrganizationInterface>
+              formik={formik}
+              name={'organization_id'}
+              label={'Select Organization'}
+              placeholder={'Select Organization'}
+              fetcher={getOrganizations}
+              renderOption={(record) => (
+                <option key={record.id} value={record.id}>
+                  {record?.name}
+                </option>
+              )}
+            />
+            <Button isDisabled={formik?.isSubmitting} colorScheme="blue" type="submit" mr="4">
+              Submit
+            </Button>
+          </form>
+        )}
+      </Box>
+    </AppLayout>
+  );
+}
+
+export default withAuthorization({
+  service: AccessServiceEnum.PROJECT,
+  entity: 'player',
+  operation: AccessOperationEnum.UPDATE,
+})(PlayerEditPage);
